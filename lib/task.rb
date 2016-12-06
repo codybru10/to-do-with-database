f119417549aa3f678caf86b805074d48feb53184
Cody Brubaker and Eric Bomblatus <cody+eric@>
2016-12-05 16:48:55 -0800

    add passing specs for front end

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,9 @@
 class Task
   attr_reader(:description, :list_id, :due_date)
-  # @@all_tasks = []
+
 
   define_method(:initialize) do |attributes|
-    @description = attributes.fetch(:description)
+    @description = attributes[:description]
     @list_id = attributes[:list_id]
     @due_date = attributes[:due_date]
   end
@@ -24,9 +24,9 @@ class Task
     DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}',  #{@list_id});")
   end
 
-def self.sort_tasks_by_date_asc (task_key)
-  DB.exec("SELECT * FROM taks WHERE list_id -= '#{task_key}' ORDER BY due_date ASC;")
-end 
+  def self.sort_tasks_by_date_asc (task_key)
+    DB.exec("SELECT * FROM taks WHERE list_id -= '#{task_key}' ORDER BY due_date ASC;")
+  end
 
   # define_singleton_method(:clear) do
   #   @@all_tasks = []
@@ -36,32 +36,3 @@ end
     self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
   end
 end
-
-
-# class Task
-#   attr_reader(:description, :list_id)
-#
-#   define_method(:initialize) do |attributes|
-#     @description = attributes.fetch(:description)
-#     @list_id = attributes.fetch(:list_id)
-#   end
-#
-#   define_singleton_method(:all) do
-#     returned_tasks = DB.exec("SELECT * FROM tasks;")
-#     tasks = []
-#     returned_tasks.each() do |task|
-#       description = task.fetch("description")
-#       list_id = task.fetch("list_id").to_i() # The information comes out of the database as a string.
-#       tasks.push(Task.new({:description => description, :list_id => list_id}))
-#     end
-#     tasks
-#   end
-#
-#   define_method(:save) do
-#     DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
-#   end
-#
-#   define_method(:==) do |another_task|
-#     self.description().==(another_task.description()).&(self.list_id().==(another_task.list_id()))
-#   end
-# end
